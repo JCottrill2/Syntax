d295c633304dd84acb801ac244e206c088289eee
jess <jcottrill1123@gmail.com>
2022-08-11 17:38:31 -0400

    initial commit

---
+++ SyntaxExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿//int answer = 4;
+//string response;
+
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+
+//else
+//{
+//    response = answer + " greater than or equal to nine";
+//}
+//Console.WriteLine(response);
+
+
+//var answer = 4;
+//var response = "";
+//if (answer < 9)
+//{
+//    response = ($"{answer} is less than nine");        
+//}
+
+//else
+//{
+//  response =  $"{answer} is greater than nine.";
+//}
+//Console.WriteLine(response);
+
+
+var answer = 4;
+var response2 = (answer < 9) ?
+    $"{answer} is less than nine" :
+    $"{answer} is greater than or equal to nine";
+
+Console.WriteLine(response2);
+
+
+
+
+
